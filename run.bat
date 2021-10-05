8542860e2e3108fd95d6b8a37be5b7a9895bdc66
Gregwar <g.passault@gmail.com>
2021-10-05 09:35:28 +0200

    Checking for Python in run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,20 @@
+@echo off
+cls
+
+goto :DOES_PYTHON_EXIST
+
+:DOES_PYTHON_EXIST
+where py && (goto :PYTHON_DOES_EXIST)
+goto :PYTHON_DOES_NOT_EXIST
+goto :EOF
+
+:PYTHON_DOES_NOT_EXIST
+echo.
+echo ERROR: Python not found: please install it before!
+echo.
+pause
+goto :EOF
+
+:PYTHON_DOES_EXIST
 py -m pip install pyserial numpy scipy zmq pyqt5 pyqtwebengine opencv-python-headless opencv-contrib-python-headless
 py -m ssl.app
