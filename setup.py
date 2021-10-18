4f4922ad91e4675749e1a97e019355251a3c8f9b
Gregwar <g.passault@gmail.com>
2021-10-18 10:50:03 +0200

    Pypi package

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="junior-ssl",
+    version="0.1",
+    author="Rhoban team",
+    author_email="team@rhoban.com",
+    description="Junior SSL - An omniwheel soccer setup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhoban/junior-ssl/",
+    packages=setuptools.find_packages(),
+    scripts=['jssl-gc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    keywords="robot holonomic omniwheel ssl robocup junior soccer standard localized tracking",
+    install_requires=[
+        "pyserial",
+        "numpy",
+        "zmq",
+        "pyqt5",
+        "pyqtwebengine",
+        "opencv-python-headless",
+        "opencv-contrib-python-headless"
+    ],
+    python_requires='>=3.6',
+)
