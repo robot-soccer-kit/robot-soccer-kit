b6174090c10d300c55214179bdf77dc45777f12d
Gregwar <g.passault@gmail.com>
2021-10-28 09:42:07 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def package_files(directory):
 
 setuptools.setup(
     name="junior-ssl",
-    version="0.2.0",
+    version="0.2.1",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Junior SSL - An omniwheel soccer setup",
