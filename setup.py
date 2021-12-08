edd2bb66a53b88c7a778ad9500bc57199ea501df
unknown <o-User-DawinAlt@info-p-lp-35.iut.bx1>
2021-12-08 13:14:10 +0100

    Ignoring pycache

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
-            if '__pycache__' not in filename:
+            if '__pycache__' not in path:
                 paths.append(os.path.join('..', path, filename))
     return paths
 
