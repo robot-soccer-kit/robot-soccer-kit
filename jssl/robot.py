8f7f254dd99887a4bce3c18a8025befc65c65142
unknown <o-User-DawinAlt@info-p-lp-35.iut.bx1>
2021-12-09 18:06:34 +0100

    Even dimmer LEDs

--- jssl/robot.py
+++ jssl/robot.py
@@ -127,9 +127,9 @@ class Robot:
         if marker is None:
             self.ledsColor = None
         elif marker.startswith('red'):
-            self.ledsColor = [128, 0, 0]
+            self.ledsColor = [32, 0, 0]
         elif marker.startswith('blue'):
-            self.ledsColor = [0, 0, 128]
+            self.ledsColor = [0, 0, 32]
         else:
             self.ledsColor = None
 
