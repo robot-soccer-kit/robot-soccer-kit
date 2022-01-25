5acacd39be12353de05079385e476a9b8d94815a
Gregwar <g.passault@gmail.com>
2022-01-25 17:31:05 +0100

    Update prepare.sh

--- firmware/prepare.sh
+++ firmware/prepare.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-if [ -d "RhobanMaple" ]; then
+if [ -d "Maple" ]; then
     cd Maple &&
-    git pull &&
-    cd Rhock &&
-    git pull
+    git pull 
 else
-    git clone https://github.com/Rhoban/Maple.git --depth=1 &&
-    cd Maple &&
-    git clone https://github.com/Rhoban/Rhock.git --depth=1
+    git clone https://github.com/Rhoban/Maple.git --depth=1 
 fi
