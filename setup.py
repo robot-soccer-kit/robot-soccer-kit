5e3770e821454b8d6f44a8a113c8091f8c04fb5f
Gregwar <g.passault@gmail.com>
2022-05-13 14:43:40 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def package_files(directory):
 
 setuptools.setup(
     name="robot-soccer-kit",
-    version="1.0.10",
+    version="1.1.0",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Robot Soccer Kit",
