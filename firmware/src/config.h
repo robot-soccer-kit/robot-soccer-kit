47588568708b5de3cef93bb69ef96ea0c51f7713
Gregwar <g.passault@gmail.com>
2022-09-20 12:57:59 +0200

    Detailing options

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -6,29 +6,40 @@
 #define KICK_PIN 33
 
 // Wheels gear reduction ratio
-#define REDUCTION_RATIO  150
+#define REDUCTION_RATIO  100
 
 // Cycles per revolution [cycles/turn]
 // The minus sign is here because encoder increments in reverted order with respect to pwm
-#define WHEELS_CPR   -(7*4*REDUCTION_RATIO)
+#define WHEELS_CPR  -(7*4*REDUCTION_RATIO)
 
 // Window size for speed estimation (since we are servoing at 1Khz; this can be considered as the amount of
 // milliseconds used for finite differences speed estimation)
 #define SPEED_WINDOW 5
 
+// Do the motors make the robot rotating positively?
+#define MOTORS_ROTATION_SIGN -1
+
+// Board revision
+#define BOARD_REV2
+
 // Wheels orientations [deg]
 // In chassis frame, the front axis is the x axis running through the kicker of the robot
-// Those are orientation of (positive PWM) driving direction of the wheels (direct angle from chassis x to the
-// vector)
+// Those are orientation of the driving direction of the wheels, assuming they rotate in the
+// direction that makes the robot rotate p=ositively
+#ifdef BOARD_REV2
 #define WHEEL1_ALPHA    -90
 #define WHEEL2_ALPHA    30
 #define WHEEL3_ALPHA    150
-
-// Sign (if inversion of the motors is needed)
-#define MOTORS_SIGN 1
+#else
+#define WHEEL1_ALPHA    150
+#define WHEEL2_ALPHA    -90
+#define WHEEL3_ALPHA    30
+#endif
 
 // This define uses BRAKE mode to drive the motors
-#define MOTORS_BRAKE
+// #ifdef BOARD_REV2
+// #define MOTORS_BRAKE
+// #endif
 
 // Wheel radius [m]
 #define MODEL_WHEEL_RADIUS    0.035
@@ -37,9 +48,10 @@
 #define MODEL_ROBOT_RADIUS    0.0595
 
 // Maximum wheels speed [rad/s]
-#define MAX_WHEEL_SPEEDS 8
+#define MAX_WHEEL_SPEEDS 16
 
 // Motors wiring pins (PWM for the two directions and A/B encoders)
+#ifdef BOARD_REV2
 #define MOTOR1_PWM1    13
 #define MOTOR1_PWM2    14
 #define MOTOR1_ENC1    25
@@ -54,6 +66,22 @@
 #define MOTOR3_PWM2    23
 #define MOTOR3_ENC1    4
 #define MOTOR3_ENC2    16
+#else
+#define MOTOR1_PWM1    13
+#define MOTOR1_PWM2    14
+#define MOTOR1_ENC1    4
+#define MOTOR1_ENC2    16
+
+#define MOTOR2_PWM1    17
+#define MOTOR2_PWM2    18
+#define MOTOR2_ENC1    19
+#define MOTOR2_ENC2    21
+
+#define MOTOR3_PWM1    22
+#define MOTOR3_PWM2    23
+#define MOTOR3_ENC1    25
+#define MOTOR3_ENC2    26
+#endif
 
 // LEDs pin (6 RGB addressable WS2812 LEDs)
 #define LEDS  27
