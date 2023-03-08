e31dbfd1344de3c2d75235855ca19f37e48efe21
Cyprien-Brocaire <pecheur.scander0b@icloud.com>
2023-03-08 09:03:13 +0100

    typing simulator

--- rsk/simulator.py
+++ rsk/simulator.py
@@ -5,13 +5,160 @@ from numpy.linalg import norm
 from math import dist
 from . import kinematics, utils, constants, state
 
+from collections.abc import Callable
+import numpy.typing as npt
+
+
+class SimulatedObject:
+    def __init__(self, marker: str, position: npt.NDArray, radius: int, deceleration: float = 0, mass: float = 1) -> None:
+        self.marker: str = marker
+        self.radius: int = radius
+
+        self.mass: float = mass
+        self.position: npt.NDArray = np.array([float(i) for i in position])
+
+        self.velocity: npt.NDArray = np.array([0.0, 0.0, 0.0])
+        self.deceleration: float = deceleration
+
+        self.pending_actions: list(Callable) = []
+        self.sim: Simulator = None
+
+    def execute_actions(self) -> None:
+        for action in self.pending_actions:
+            action()
+        self.pending_actions = []
+
+    def update_velocity(self, dt) -> None:
+        self.velocity[:2] = utils.update_limit_variation(self.velocity[:2], np.array([0.0, 0.0]), self.deceleration * dt)
+
+    def collision_R(self, obj):
+        """
+        Given another object, computes the collision frame.
+        It returns R_collision_world
+        """
+
+        # Computing unit vectors normal and tangent to contact (self to obj)
+        normal = obj.position[:2] - self.position[:2]
+        normal = normal / norm(normal)
+        tangent = np.array([[0, -1], [1, 0]]) @ normal
+
+        return np.vstack((normal, tangent))
+
+    def collision(self, obj) -> None:
+        R_collision_world = self.collision_R(obj)
+
+        # Velocities expressed in the collision frame
+        self_velocity_collision = R_collision_world @ self.velocity[:2]
+        obj_velocity_collision = R_collision_world @ obj.velocity[:2]
+
+        # Updating velocities using elastic collision
+        u1 = self_velocity_collision[0]
+        u2 = obj_velocity_collision[0]
+        m1 = self.mass
+        m2 = obj.mass
+        Cr = 0.5
+
+        self_velocity_collision[0] = (m1 * u1 + m2 * u2 + m2 * Cr * (u2 - u1)) / (m1 + m2)
+        obj_velocity_collision[0] = (m1 * u1 + m2 * u2 + m1 * Cr * (u1 - u2)) / (m1 + m2)
+
+        # Velocities back in the world frame
+        self.velocity[:2] = R_collision_world.T @ self_velocity_collision
+        obj.velocity[:2] = R_collision_world.T @ obj_velocity_collision
+
+
+class Robots:
+    def __init__(self, state=None) -> None:
+        self.robots: dict = {}
+        self.robots_by_marker: dict[str, Robot] = {}
+        self.state: state.State = state
+
+        for marker, position in zip(
+            ["green1", "green2", "blue1", "blue2"],
+            [[-0.5, 0.5, 0], [-0.5, -0.5, 0], [0.5, 0.5, 0], [0.5, -0.5, 0]],
+        ):
+            robot: Robot = Robot(marker, position)
+            self.robots_by_marker[marker] = robot
+
+    def should_restore_leds(self, robot: str) -> None:
+        pass
+
+    def ports(self) -> list[int]:
+        return [0, 0, 0, 0]
+
+    def get_robots(self) -> dict:
+        """
+        Gets robots informations.
+
+        :return dict: information about robots
+        """
+        data = {}
+        for entry in self.robots:
+            last_detection = None
+            if self.robots[entry].marker in self.detection.last_updates:
+                last_detection = time.time() - self.detection.last_updates[self.robots[entry].marker]
+
+            data[entry] = {
+                "state": self.robots[entry].state,
+                "marker": self.robots[entry].marker,
+                "last_detection": last_detection,
+                "last_message": time.time() - self.robots[entry].last_message
+                if self.robots[entry].last_message is not None
+                else None,
+            }
+
+        return data
+
+
+class Robot(SimulatedObject):
+    def __init__(self, marker: str, position: npt.NDArray) -> None:
+        super().__init__(marker, position, kinematics.robot_radius, 0, constants.robot_mass)
+        self.control_cmd: npt.NDArray = np.array([0.0, 0.0, 0.0])
+
+    def control(self, dx: float, dy: float, dturn: float) -> None:
+        self.control_cmd = kinematics.clip_target_order(np.array([dx, dy, dturn]))
+
+    def kick(self, power: float = 1.0) -> None:
+        self.pending_actions.append(lambda: self.compute_kick(power))
+
+    def compute_kick(self, power: float) -> None:
+        # Robot to ball vector, expressed in world
+        ball_world = self.sim.objects["ball"].position[:2]
+        T_world_robot = utils.frame(tuple(self.position))
+        T_robot_world = utils.frame_inv(T_world_robot)
+        ball_robot = utils.frame_transform(T_robot_world, ball_world)
+
+        if utils.in_rectangle(
+            ball_robot,
+            [self.radius - constants.kicker_x_tolerance, -constants.kicker_y_tolerance],
+            [self.radius + constants.kicker_x_tolerance, constants.kicker_y_tolerance],
+        ):
+            # TODO: Move in constants
+            ball_speed_robot = [np.clip(power, 0, 1) * np.random.normal(0.8, 0.1), 0]
+            self.sim.objects["ball"].velocity[:2] = T_world_robot[:2, :2] @ ball_speed_robot
+
+    def update_velocity(self, dt: float) -> None:
+        target_velocity_robot = self.control_cmd
+
+        T_world_robot = utils.frame(tuple(self.position))
+        target_velocity_world = T_world_robot[:2, :2] @ target_velocity_robot[:2]
+
+        self.velocity[:2] = utils.update_limit_variation(
+            self.velocity[:2], target_velocity_world, constants.max_linear_acceleration * dt
+        )
+        self.velocity[2:] = utils.update_limit_variation(
+            self.velocity[2:], target_velocity_robot[2:], constants.max_angular_accceleration * dt
+        )
+
+    def leds(self, r: int, g: int, b: int) -> None:
+        pass
+
 
 class Simulator:
-    def __init__(self, robots, state):
-        self.state = state
-        self.robots = robots
+    def __init__(self, robots: Robots, state: state.State):
+        self.state: state.State = state
+        self.robots: Robots = robots
 
-        self.objects: dict = {}
+        self.objects: dict[str, Robot] = {}
 
         # Creating the ball
         self.add_object(
@@ -20,23 +167,20 @@ class Simulator:
 
         self.refresh_robots()
 
-        self.on_tick = None
-
-        self.simu_thread = threading.Thread(target=lambda: self.thread())
+        self.simu_thread: threading.Thread = threading.Thread(target=lambda: self.thread())
         self.simu_thread.start()
 
-        self.period = None
-        self.lock = threading.Lock()
+        self.lock: threading.Lock = threading.Lock()
 
-    def add_object(self, object) -> None:
+    def add_object(self, object: SimulatedObject) -> None:
         self.objects[object.marker] = object
         object.sim = self
 
-    def refresh_robots(self):
+    def refresh_robots(self) -> None:
         for object in self.robots.robots_by_marker.values():
             self.add_object(object)
 
-    def thread(self):
+    def thread(self) -> None:
         last_time = time.time()
         while True:
             self.dt = -(last_time - (last_time := time.time()))
@@ -75,170 +219,16 @@ class Simulator:
                 obj.position = obj.position + (obj.velocity * self.dt)
 
             # TODO: Remove this
-            if np.linalg.norm(self.objects["ball"].position[:2]) > 1.5:
+            if np.linalg.norm(self.objects["ball"].position[:2]) > 0.7:
                 self.objects["ball"].position[:3] = [0.0, 0.0, 0.0]
                 self.objects["ball"].velocity[:3] = [0.0, 0.0, 0.0]
 
-            if self.on_tick is not None:
-                self.on_tick()
-
             self.push()
 
-            # TODO: Configure the frequency
-            time.sleep(0.01)
-
-    def push(self):
+    def push(self) -> None:
         for marker in self.objects:
             pos = self.objects[marker].position
             if marker == "ball":
                 self.state.set_ball(pos[:2].tolist())
             else:
                 self.state.set_marker(marker, pos[:2].tolist(), pos[2])
-
-
-class Robots:
-    def __init__(self, state=None):
-        self.robots: dict = {}
-        self.robots_by_marker: dict = {}
-        self.state: state.State = state
-
-        for marker, position in zip(
-            ["green1", "green2", "blue1", "blue2"],
-            [[-0.5, 0.5, 0], [-0.5, -0.5, 0], [0.5, 0.5, 0], [0.5, -0.5, 0]],
-        ):
-            robot = Robot(marker, position)
-            self.robots_by_marker[marker] = robot
-
-    def should_restore_leds(self, robot: str):
-        pass
-
-    def ports(self):
-        return [0, 0, 0, 0]
-
-    def get_robots(self) -> dict:
-        """
-        Gets robots informations.
-
-        :return dict: information about robots
-        """
-        data = {}
-        for entry in self.robots:
-            last_detection = None
-            if self.robots[entry].marker in self.detection.last_updates:
-                last_detection = time.time() - self.detection.last_updates[self.robots[entry].marker]
-
-            data[entry] = {
-                "state": self.robots[entry].state,
-                "marker": self.robots[entry].marker,
-                "last_detection": last_detection,
-                "last_message": time.time() - self.robots[entry].last_message
-                if self.robots[entry].last_message is not None
-                else None,
-            }
-
-        return data
-
-
-class SimulatedObject:
-    def __init__(self, marker, position, radius, deceleration: float = 0, mass: float = 1):
-        self.marker = marker
-        self.radius = radius
-
-        self.mass = mass
-        self.position = np.array([float(i) for i in position])
-
-        self.velocity = np.array([0.0, 0.0, 0.0])
-        self.deceleration = deceleration
-
-        self.pending_actions = []
-        self.sim: Simulator = None
-
-    def execute_actions(self):
-        for action in self.pending_actions:
-            action()
-        self.pending_actions = []
-
-    def update_velocity(self, dt):
-        self.velocity[:2] = utils.update_limit_variation(self.velocity[:2], np.array([0.0, 0.0]), self.deceleration * dt)
-
-    def collision_R(self, obj):
-        """
-        Given another object, computes the collision frame.
-        It returns R_collision_world
-        """
-
-        # Computing unit vectors normal and tangent to contact (self to obj)
-        normal = obj.position[:2] - self.position[:2]
-        normal = normal / norm(normal)
-        tangent = np.array([[0, -1], [1, 0]]) @ normal
-
-        return np.vstack((normal, tangent))
-
-    def collision(self, obj):
-        print("Collision")
-
-        R_collision_world = self.collision_R(obj)
-
-        # Velocities expressed in the collision frame
-        self_velocity_collision = R_collision_world @ self.velocity[:2]
-        obj_velocity_collision = R_collision_world @ obj.velocity[:2]
-
-        # Updating velocities using elastic collision
-        u1 = self_velocity_collision[0]
-        u2 = obj_velocity_collision[0]
-        m1 = self.mass
-        m2 = obj.mass
-        Cr = 0.5
-
-        self_velocity_collision[0] = (m1 * u1 + m2 * u2 + m2 * Cr * (u2 - u1)) / (m1 + m2)
-        obj_velocity_collision[0] = (m1 * u1 + m2 * u2 + m1 * Cr * (u1 - u2)) / (m1 + m2)
-
-        # Velocities back in the world frame
-        self.velocity[:2] = R_collision_world.T @ self_velocity_collision
-        obj.velocity[:2] = R_collision_world.T @ obj_velocity_collision
-
-
-class Robot(SimulatedObject):
-    def __init__(self, marker, position):
-        super().__init__(marker, position, kinematics.robot_radius, 0, constants.robot_mass)
-        self.control_cmd = np.array([0.0, 0.0, 0.0])
-
-    def control(self, dx: float, dy: float, dturn: float):
-        self.control_cmd = kinematics.clip_target_order(np.array([dx, dy, dturn]))
-
-    def kick(self, power: float = 1.0):
-        self.pending_actions.append(lambda: self.compute_kick(power))
-
-    def compute_kick(self, power):
-        # Robot to ball vector, expressed in world
-        ball_world = self.sim.objects["ball"].position[:2]
-        T_world_robot = utils.frame(tuple(self.position))
-        T_robot_world = utils.frame_inv(T_world_robot)
-        ball_robot = utils.frame_transform(T_robot_world, ball_world)
-
-        if utils.in_rectangle(
-            ball_robot,
-            [self.radius - constants.kicker_x_tolerance, -constants.kicker_y_tolerance],
-            [self.radius + constants.kicker_x_tolerance, constants.kicker_y_tolerance],
-        ):
-            print("Kick_Valid")
-
-            # TODO: Move in constants
-            ball_speed_robot = [np.clip(power, 0, 1) * np.random.normal(0.8, 0.1), 0]
-            self.sim.objects["ball"].velocity[:2] = T_world_robot[:2, :2] @ ball_speed_robot
-
-    def update_velocity(self, dt):
-        target_velocity_robot = self.control_cmd
-
-        T_world_robot = utils.frame(tuple(self.position))
-        target_velocity_world = T_world_robot[:2, :2] @ target_velocity_robot[:2]
-
-        self.velocity[:2] = utils.update_limit_variation(
-            self.velocity[:2], target_velocity_world, constants.max_linear_acceleration * dt
-        )
-        self.velocity[2:] = utils.update_limit_variation(
-            self.velocity[2:], target_velocity_robot[2:], constants.max_angular_accceleration * dt
-        )
-
-    def leds(self, r: int, g: int, b: int):
-        pass
