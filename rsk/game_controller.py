3160edea260c7775021c5d86a2e8ab8bb5aa32b7
Cyprien-Brocaire <pecheur.scander0b@icloud.com>
2023-10-13 15:55:31 +0200

    Enabling remote arbiter with -r

--- rsk/game_controller.py
+++ rsk/game_controller.py
@@ -22,14 +22,16 @@ logging.getLogger("robot-soccer-kit").info("Starting robot-soccer-kit Game Contr
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", "-p", type=str, default="7070")
-parser.add_argument("--ip", "-ip", type=str, default="192.168.0.77")
+parser.add_argument("--ip", "-ip", type=str, default="127.0.0.1")
 parser.add_argument("--simulated", "-s", action="store_true")
+parser.add_argument("--remote", "-r", action="store_true")
 args = parser.parse_args()
 
 auth = HTTPBasicAuth()
 users = {
-    "admin": 'pbkdf2:sha256:600000$R7cU35KCJOOT0O99$9657bd0616574504eb15c2542e25903c79beb42a47ef714d02270e9dc250ec13',
+    "admin": 'scrypt:32768:8:1$j9gQIiONIsQ6wenA$7da7778a7ff169a35426ccbb3fc7b5bb5d766a59bb60273d0e08d63d1c36d95c6522b61a8320d5ba6d43ecc247d14fffc367fe2634e9f31311e53f7b09f288d1',
 }
+
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
@@ -46,7 +48,12 @@ static = os.path.dirname(__file__) + "/static/"
 app = Flask("Game controller", static_folder=static)
 CORS(app)
 
+static = os.path.dirname(__file__) + "/static/"
+app_public = Flask("Game controller", static_folder=static)
+CORS(app_public)
+
 @app.route("/api", methods=["GET"])
+@app_public.route("/api", methods=["GET"])
 def handle_api():
     global has_client
     has_client = True
@@ -70,11 +77,14 @@ def handle_api():
         return jsonify([0, "Error while processing command"])
 
 
-@app.route("/", methods=["GET"])
+@app_public.route("/", methods=["GET"])
 @auth.login_required
-def main():
+def mainPub():
     return send_from_directory(static, "index.html")
 
+@app.route("/", methods=["GET"])
+def main():
+    return send_from_directory(static, "index.html")
 
 def run_browser():
     # If no request arrived for 3s, starting a browser interface on local
@@ -82,9 +92,15 @@ def run_browser():
     if not has_client:
         webbrowser.open(f"http://{args.ip}:{args.port}")
 
+def run_waitress_public():
+    waitress.serve(app_public, listen="%s:%s" % ("192.168.0.77", args.port), threads=8)
 
 thread = threading.Thread(target=run_browser)
 thread.start()
+
+if args.remote:
+    thread = threading.Thread(target=run_waitress_public)
+    thread.start()
 
 # Serving forever
 waitress.serve(app, listen="%s:%s" % (args.ip, args.port), threads=8)
