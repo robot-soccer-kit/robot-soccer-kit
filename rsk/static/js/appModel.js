34f0102decb28425506acd9dc03979a812fc9520
Cyprien-Brocaire <pecheur.scander0b@icloud.com>
2023-11-08 11:16:19 +0100

    add appModel for remote referee

---
+++ rsk/static/js/appModel.js
@@ -0,0 +1,66 @@
+class APIBackend {
+    constructor(url) {
+        return new Proxy(this, {
+            get: function get(target, name) {
+                return function (...args) {
+                    let last = args.length - 1;
+                    let callback = null;
+                    if (typeof (args[last]) == 'function') {
+                        callback = args.pop();
+                    }
+
+                    $.get({"url" : url, "data" :{ 'command': name, 'args': JSON.stringify(args)}, 'success':function (result) {
+                        $('.waiting-screen').css("display", 'None')
+                        $('.loader-robot').css("animation-play-state", 'paused')
+                        if (result) {
+                            if (result[0]) {
+                                if (callback) {
+                                    callback(result[1]);
+                                }
+                            } else {
+                                console.log('Error: ' + result[1]);
+                            }
+                        }
+                    }, "timeout": 1000}).fail(function(){
+                        $('.waiting-screen').css("display", 'inherit')
+                        $('.loader-robot').css("animation-play-state", 'running')
+                    });;
+                }
+            }
+        });
+    }
+}
+
+
+$(document).ready(function () {
+    // Backend initialization
+    var backend = new APIBackend('http://IP_HERE:7070/api');
+    backend.is_simulated(function (simulated) {
+        if (simulated) {
+            console.log("SIMULATION")
+            $('.not_show_simulated').css("display", 'none')
+            simulator_initialize(backend, true)
+        }else{
+           console.log("REEL")
+           $('.show_simulated').css("display", 'none')
+           video_initialize(backend); 
+           simulator_initialize(backend, false)
+        }
+    })
+
+    backend.is_competition(function (competition) {
+        if (competition) {
+            $('.competition-mode').show();
+            competition_initialize(backend);
+        }
+    });
+    
+    robots_initialize(backend);
+    control_initialize(backend);
+    referee_initialize(backend);
+
+    // (dev) Reload the window
+    $('.reload').click(function () {
+        window.location.reload();
+    });
+});
