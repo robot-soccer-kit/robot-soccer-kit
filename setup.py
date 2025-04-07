47d91b40e89134b43f98f7ed34fd369cb007d4bc
Grégoire Passault <g.passault@gmail.com>
2025-04-07 11:44:08 +0200

    2.3.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def package_files(directory):
 
 setuptools.setup(
     name="robot_soccer_kit",
-    version="2.3.3",
+    version="2.3.4",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Robot Soccer Kit",
